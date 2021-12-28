596d9a176b768db4fba815d5ac1c80f2d7069b43
xand <xand@xandgate.com>
2021-12-28 00:23:11 -0500

    Now able to autogen lens classes with TH

--- NanoLens/TH.hs
+++ NanoLens/TH.hs
@@ -8,11 +8,13 @@ module NanoLens.TH where
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State
+import Data.Char
 import Data.Eq
 import Data.Functor
 import Data.Functor.Identity
 import Data.Functor.Const
 import Data.Function
+import Data.Maybe
 import Data.List
 import GHC.Int
 import Text.Show
@@ -25,10 +27,41 @@ import Language.Haskell.TH.Lib
 
 
 
+-- | Automate all the legwork of generating nanolenses. Any other
+-- top-level nanolens generators should call to this with their rules.
+--
+nanolensify :: Rules -> Name -> Q [Dec]
+nanolensify rules tyName = do
+    (con, tyVars) <- reifyConAndStuff
+
+    let tyArgs = map tyVarToVar tyVars where
+            tyVarToVar (PlainTV nm)     = VarT nm
+            tyVarToVar (KindedTV nm _)  = VarT nm
+        ty = tyName `conAppsT` tyArgs
+
+        fields = case con of
+            RecC nm fs -> fs
+
+    concat <$> (sequence $ genZipper con ty fields)
+  where
+    genLens con ty   = genLensSupplemental rules con ty
+    genZipper con ty = zipWith (genLens con ty) [0..]
+
+    reifyConAndStuff = do
+        TyConI tyDec <- reify tyName
+        return $ case tyDec of
+            DataD _ _ tyVars _ [con] _ -> (con, tyVars)
+
+genSimpleLenses :: Name -> Q [Dec]
+genSimpleLenses = nanolensify simpleRules
+
+genLenses :: Name -> Q [Dec]
+genLenses = nanolensify classyRules
+
 -- | Automatically generate a lens for a numbered field in a record.
 --
-genNumberedLens :: Name -> Con -> Int -> Q Dec
-genNumberedLens lensName con nr = do
+genNumberedLens :: Con -> Int -> Name -> Q Dec
+genNumberedLens con nr lensName = do
     let (conName, fields) = case con of
             RecC nm fs -> (nm, fs)
         fieldNames = map (\(nm, _, _) -> nm) fields
@@ -61,21 +94,106 @@ genNumberedLens lensName con nr = do
         [ clause argsP (normalB body) [recD] ]
     return lensDec
 
--- | Automatically derive lenses for all fields in a datatype.
+-- | Generate a lens with supplemental equipment.
 --
-genAllLenses :: Name -> Q [Dec]
-genAllLenses tyName = do
-    TyConI tyDec <- reify tyName
-    case tyDec of
-        DataD _ _ _ _ [con] _ -> go con
+-- Some lenses should be defined as class methods to prevent namespace
+-- pollution. That will be taken care of here.
+--
+genLensSupplemental
+    :: Rules
+    -> Con
+    -> Type
+    -> Int
+    -> (Name, Bang, Type)
+    -> Q [Dec]
+genLensSupplemental rules con s nr (fieldName, _, fieldTy) = do
+    fieldClassD <- maybeGenFieldClass
+    sequence $ lensInstanceD : fieldClassD
   where
-    go con@(RecC nm fields) = do
-        let lensNames = genLensNames fields
-        lenses <- sequence $
-            zipWith (\nm nr -> genNumberedLens nm con nr) lensNames [0..]
-        return lenses
+    maybeGenFieldClass :: Q [Q Dec]
+    maybeGenFieldClass
+      | generateClasses rules = do
+        classExists <- isJust <$> lookupTypeName (show className)
+        return $ if classExists
+            then []
+            else [genFieldClass className lensName]
+      | otherwise = return []
 
-    genLensNames fields =
-        zipWith (\n _ -> mkName $ "lens" ++ show @Int n) [0..] fields
+    lensFunD = genNumberedLens con nr lensName
+    lensInstanceD
+      | generateClasses rules =
+        genFieldInstance s fieldTy className lensFunD
+      | otherwise = lensFunD
+
+    lensName    = fieldNamer rules fieldName
+    className   = classNamer rules lensName
+
+-- | Generate a multi-param type class for a field with a particular name.
+--
+-- For example:
+--  >>> data Bar = Bar { _foo :: Field1 }
+--  >>>
+--  >>> class HasFoo s a where
+--  >>>     foo :: Lens' s a
+--  >>>
+--  >>> instance HasFoo Bar Field1 where
+--  >>>     foo = lens (_foo) (\x b -> x {_foo = b})
+--
+genFieldClass :: Name -> Name -> Q Dec
+genFieldClass className lensName = do
+    s <- newName "s"
+    a <- newName "a"
+
+    let sTyVar  = PlainTV s
+        sTy     = VarT s
+        aTyVar  = PlainTV a
+        aTy     = VarT a
+
+    classD (cxt []) className [sTyVar, aTyVar] []
+        $ sigD lensName (return $ ''Lens' `conAppsT` [sTy, aTy])
+        : []
+
+-- | Generate the instance corresponding to a field type class.
+--
+genFieldInstance :: Type -> Type -> Name -> Q Dec -> Q Dec
+genFieldInstance ty fieldTy className lensFunD = do
+    instanceD (cxt []) (return $ className `conAppsT` [ty, fieldTy])
+        [ lensFunD ]
+
+
+data Rules = Rules
+    { generateClasses   :: Bool
+    , classNamer        :: ClassNamer
+    , fieldNamer        :: FieldNamer }
+
+type ClassNamer = Name -> Name
+type FieldNamer = Name -> Name
+
+simpleRules :: Rules
+simpleRules = Rules
+    { generateClasses   = False
+    , classNamer        = const (mkName "")
+    , fieldNamer        = underscorize }
+
+underscorize nm = case nameBase nm of
+    '_':x:xs    -> mkName (toLower x:xs)
+    _           -> mkName ""
+
+classyRules :: Rules
+classyRules = Rules
+    { generateClasses   = True
+    , classNamer        = classyNamer
+    , fieldNamer        = underscorize }
+  where
+    classyNamer nm = case nameBase nm of
+        x:xs -> mkName $ "Has" ++ (toUpper x:xs)
+        []   -> mkName ""
+
+
+-- | Apply a list of TH type arguments to a type constructor.
+--
+conAppsT :: Name -> [Type] -> Type
+conAppsT nm = foldl AppT (ConT nm)
+
 
 
